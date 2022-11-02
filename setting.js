d1cbe8a86327dc31c1c72deeb16941cbe8df6b8f
alim151104 <117247603+alim151104@users.noreply.github.com>
2022-11-02 21:17:28 +0700

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,52 @@
+ {
+ "contactOwner": "62895368659400", 
+ "botName": "Venzy Bot", 
+ "ownerName": "Venzy Owner", 
+ "sessionName": "session", 
+ "footer": "Venzy Bot © 2022", 
+ "packname": "Venzy Bot", 
+ "author": "Venzy Owner", 
+
+ "youtube": "YouTube", 
+ "github": "Github", 
+ "instagram": "Instagram", 
+ "tiktok": "TikTok", 
+ "website": "Website", 
+ "email": "Email", 
+ "gender": "Gender/Jenis Kelamin", 
+ "agama": "Agama", 
+ "tanggallahir": "Tanggal Lahir", 
+ "umur": "Umur", 
+ "kelas": "Kelas", 
+ "hobi": "Hobi", 
+ "sifat": "Sifat", 
+ "tinggal": "Tempat Tinggal", 
+ "suka": "Kesukaan", 
+ "benci": "Kebencian", 
+ 
+  "api_alpha": "https://alphabot-api.herokuapp.com", 
+  "apikey_alpha": "APIKEY", 
+  
+  "api_ronzz": "https://ronzz-api.herokuapp.com", 
+  "apikey_ronzz": "APIKEY", 
+ 
+ "api_zenz": "lexxybotygy", 
+ "apikey_antlatic": "APIKEY", 
+
+ "payment": {
+    "qris": {
+      "link_nya": "Link Qris/Foto qris", 
+      "atas_nama": "Nama" 
+    },
+    "dana": {
+      "nomer": "Nomor Dana", 
+      "atas_nama": "Nama" 
+    }},
+    
+  "pathQris": "./temp/media/qris.jpg", 
+  "pathCont": "./temp/media/contributor.jpg", 
+  "pathBc": "./temp/media/bc.jpg", 
+  "pathThumb": "./temp/media/logoNya.jpg", 
+  "pathThumb2": "./temp/media/logoNya.jpg", 
+  "pathMenfes": "./temp/media/menfes.jpg" 
+  }
